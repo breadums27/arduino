b60904416ac650c8805b77e74c01818d9d815fb5
breadums27 <cornelus.nelson@myds.us>
2017-05-31 10:30:43 -0700

    Add files via upload

---
+++ starwars.ino
@@ -0,0 +1,162 @@
+#include <LiquidCrystal.h> // includes the LiquidCrystal Library 
+
+
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7); // Creates an LC object. Parameters: (rs, enable, d4, d5, d6, d7) 
+ 
+const int c = 261;
+const int d = 294;
+const int e = 329;
+const int f = 349;
+const int g = 391;
+const int gS = 415;
+const int a = 440;
+const int aS = 455;
+const int b = 466;
+const int cH = 523;
+const int cSH = 554;
+const int dH = 587;
+const int dSH = 622;
+const int eH = 659;
+const int fH = 698;
+const int fSH = 740;
+const int gH = 784;
+const int gSH = 830;
+const int aH = 880;
+ 
+const int buzzerPin = 8;
+const int ledPin1 = 12;
+const int ledPin2 = 13;
+ 
+int counter = 0;
+int val = 0;
+void setup()
+{
+  //Setup pin modes
+  lcd.begin(16,2); // Initializes the interface to the LCD screen, and specifies the dimensions (width and height) of the display
+  pinMode(9, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(12, INPUT);
+  lcd.print("Star Wars Theme");
+}
+ 
+void loop()
+{
+ val = digitalRead(12);  // read input value
+  if (val == HIGH) {         // check if the input is HIGH (button released)
+    digitalWrite(9, LOW);  // turn LED OFF
+  } else {
+    digitalWrite(9, HIGH);  // turn LED ON
+  }
+ 
+  //Play first section
+  firstSection();
+ 
+  //Play second section
+  secondSection();
+ 
+  //Variant 1
+  beep(f, 250);  
+  beep(gS, 500);  
+  beep(f, 350);  
+  beep(a, 125);
+  beep(cH, 500);
+  beep(a, 375);  
+  beep(cH, 125);
+  beep(eH, 650);
+ 
+  delay(500);
+ 
+  //Repeat second section
+  secondSection();
+ 
+  //Variant 2
+  beep(f, 250);  
+  beep(gS, 500);  
+  beep(f, 375);  
+  beep(cH, 125);
+  beep(a, 500);  
+  beep(f, 375);  
+  beep(cH, 125);
+  beep(a, 650);  
+ 
+  delay(650);
+}
+ 
+void beep(int note, int duration)
+{
+  //Play tone on buzzerPin
+  tone(9, note, duration);
+ 
+  //Play different LED depending on value of 'counter'
+  if(counter % 2 == 0)
+  {
+    digitalWrite(13, HIGH);
+    delay(duration);
+    digitalWrite(13, LOW);
+  }else
+  {
+    digitalWrite(13, HIGH);
+    delay(duration);
+    digitalWrite(13, LOW);
+  }
+ 
+  //Stop tone on buzzerPin
+  noTone(9);
+ 
+  delay(50);
+ 
+  //Increment counter
+  counter++;
+}
+ 
+void firstSection()
+{
+  beep(a, 500);
+  beep(a, 500);    
+  beep(a, 500);
+  beep(f, 350);
+  beep(cH, 150);  
+  beep(a, 500);
+  beep(f, 350);
+  beep(cH, 150);
+  beep(a, 650);
+ 
+  delay(500);
+ 
+  beep(eH, 500);
+  beep(eH, 500);
+  beep(eH, 500);  
+  beep(fH, 350);
+  beep(cH, 150);
+  beep(gS, 500);
+  beep(f, 350);
+  beep(cH, 150);
+  beep(a, 650);
+ 
+  delay(500);
+}
+ 
+void secondSection()
+{
+  beep(aH, 500);
+  beep(a, 300);
+  beep(a, 150);
+  beep(aH, 500);
+  beep(gSH, 325);
+  beep(gH, 175);
+  beep(fSH, 125);
+  beep(fH, 125);    
+  beep(fSH, 250);
+ 
+  delay(325);
+ 
+  beep(aS, 250);
+  beep(dSH, 500);
+  beep(dH, 325);  
+  beep(cSH, 175);  
+  beep(cH, 125);  
+  beep(b, 125);  
+  beep(cH, 250);  
+ 
+  delay(350);
+}
